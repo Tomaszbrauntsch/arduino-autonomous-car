4426e29f32ecd3ce579d93cacdeec2d2d2437b96
Tomaszbrauntsch <43079538+Tomaszbrauntsch@users.noreply.github.com>
2018-11-29 12:49:38 -0500

    added-leds-buzzer
    Added two white leds in the back and changed the red and blue front leds to yellow. Added a buzzer that makes noise during the if and else statement. Changed turning delay from 3500 to 2550

--- Initial_Robot.ino
+++ Initial_Robot.ino
@@ -5,12 +5,17 @@ Servo mywhiteservo; //Initializes 2nd servo
 //ultrasonic Pins
 const int pingPin = 6; //Trigger pin = 6
 const int echoPin = 5; // Echo pin = 5
-const int LED_B = 3;
-const int LED_R = 9;
+const int LED_YL = 3;
+const int LED_YR = 9;
+const int LED_WL = 10;
+const int LED_WR = 11;
+const int buzzpin = 12;
 void setup(){
   Serial.begin(9600);
-  pinMode(LED_B, OUTPUT);
-  pinMode(LED_R, OUTPUT);
+  pinMode(LED_YL, OUTPUT);
+  pinMode(LED_YR, OUTPUT);
+  pinMode(LED_WL, OUTPUT);
+  pinMode(LED_WR, OUTPUT);
 }
 void loop(){
 //Ultrasonic Sensor settings
@@ -33,19 +38,31 @@ if (inches > 24 ){
   mywhiteservo.attach(7);
   myblueservo.write(160);
   mywhiteservo.write(20);
-  digitalWrite(LED_B, HIGH);
+  digitalWrite(LED_YL, HIGH);
+  tone(buzzpin,500,250);
   delay(250);
-  digitalWrite(LED_B, LOW);
-  digitalWrite(LED_R, HIGH);
+  digitalWrite(LED_YL, LOW);
+  tone(buzzpin,600,250);
+  digitalWrite(LED_YR, HIGH);
+  tone(buzzpin,750,250);
   delay(250);
-  digitalWrite(LED_R, LOW);
+  tone(buzzpin,1000,250);
+  digitalWrite(LED_YR, LOW);
 }
 //Drive Backwards and turns 90° Clockwise
 else
   {
   mywhiteservo.write(160);
   myblueservo.write(20);
-  delay(3500);
+  digitalWrite(LED_WL,HIGH);
+  tone(buzzpin,1500,250);
+  digitalWrite(LED_WR,HIGH);
+  tone(buzzpin,1500,250);
+  delay(2550);
+  digitalWrite(LED_WL,LOW);
+  tone(buzzpin,1500,250);
+  digitalWrite(LED_WR,LOW);
+  tone(buzzpin,1500,250);
   myblueservo.detach();
   mywhiteservo.detach();
 }
